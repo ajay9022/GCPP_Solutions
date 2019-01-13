d48764524a0b5a608c62566ead91b38aad5fd3b0
ajay <ajaysingh9022@gmail.com>
2019-01-13 16:56:55 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,53 @@
+/*
+    #include<iostream>
+    using namespace std;
+    int main(){
+	
+	const char *a[] = {"ge","hy"};
+	cout<<a<<" "<<&a[1]<<endl;
+	cout<<a[0]<<" "<<a[1];
+	return 0;
+    }
+*/
+/*
+#include <iostream>
+int main (void) {
+    const char *terry = "hello";
+    std::cout << terry << '\n';
+    std::cout<<*terry<<std::endl;
+    return 0;
+}
+*/
+/*
+#include <iostream>
+int main (void) {
+	int p[]={4,5,6};
+    int *terry = p;
+    std::cout << terry << '\n';
+    std::cout<<*terry<<std::endl;
+    return 0;
+}
+*/
+/*
+#include <iostream>
+int main (void) {
+	char c = 'c';
+	char *terry=&c;
+    //int *terry = p;
+    std::cout << terry << '\n';
+    std::cout<<*terry<<std::endl;
+    return 0;
+}
+*/
+/*
+#include <iostream>
+int main (void) {
+	char c[] = {'a','b','c'};
+	//char *terry=&c;
+    //int *terry = p;
+    char c[] = {'a','b','c'};
+    std::cout << c << '\n';
+    //std::cout<<*terry<<std::endl;
+    return 0;
+}
+*/
